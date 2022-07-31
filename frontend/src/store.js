8cdc5753ebca09ded0f94c934c5961a864c954df
Kamil Wójtowicz <kamilwojtowicz079@gmail.com>
2022-08-01 01:29:47 +0200

    Section 5:Implementing Redux For State Management

---
+++ frontend/src/store.js
@@ -0,0 +1,25 @@
+import { applyMiddleware } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import {
+  productListReducer,
+  productDetailsReducer,
+} from './reducers/productReducers'
+
+const reducer = {
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+}
+
+const initialState = {}
+
+const middleware = [thunk]
+
+const store = configureStore({
+  reducer: reducer,
+  preloadedState: initialState,
+  middleware: middleware,
+})
+
+export default store
