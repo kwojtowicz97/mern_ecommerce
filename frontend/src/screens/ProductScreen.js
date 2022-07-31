894debaaa503081acea68e203f3218455a3cc8b6
Kamil Wójtowicz <kamilwojtowicz079@gmail.com>
2022-07-31 16:07:32 +0200

    Section 3: Serving & Fetching Data From Express

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
 
 const ProductScreen = () => {
   const params = useParams()
-  const product = products.find((p) => p._id === params.id)
+
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${params.id}`)
+      setProduct(data)
+    }
+    fetchProduct()
+  }, [params.id])
+
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
@@ -38,7 +48,7 @@ const ProductScreen = () => {
                 <Row>
                   <Col>Price:</Col>
                   <Col>
-                    <string>${product.price}</string>
+                    <strong>${product.price}</strong>
                   </Col>
                 </Row>
               </ListGroup.Item>
